da40e60593b7b9e51bdd295180fb6aab91a1acf4
Jayin Ton <tonjayin@gmail.com>
2017-05-11 23:54:53 +0800

    tweak comment

--- Install/Log.sql
+++ Install/Log.sql
@@ -6,8 +6,8 @@ DROP TABLE IF EXISTS `cms_log_log`;
 
 CREATE TABLE `cms_log_log` (
   `id` int(10) NOT NULL auto_increment COMMENT 'ID',
-  `category` varchar(128) NOT NULL COMMENT '类别',
-  `message` varchar(4098) NOT NULL COMMENT '日志信息',
+  `category` varchar(128) NOT NULL COMMENT '日志类别',
+  `message` varchar(4098) NOT NULL COMMENT '日志内容',
   `inputtime` int NOT NULL COMMENT '添加时间',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
