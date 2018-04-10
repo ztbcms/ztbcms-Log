aab66ff4bd25e1bbfed2b4eac436a5ebafda01a4
Jayin Ton <tonjayin@gmail.com>
2018-04-10 18:36:46 +0800

    v1.0.0.2

--- Config.inc.php
+++ Config.inc.php
@@ -6,7 +6,7 @@
 
 return array(
 	//模块名称
-	'modulename' => '日志信息',
+	'modulename' => '日志',
 	//图标
 	'icon' => 'https://dn-coding-net-production-pp.qbox.me/e57af720-f26c-4f3b-90b9-88241b680b7b.png',
 	//模块简介
@@ -20,7 +20,7 @@ return array(
 	//作者邮箱
 	'authoremail' => 'admin@ztbcms.com',
 	//版本号，请不要带除数字外的其他字符
-	'version' => '1.0.0.1',
+	'version' => '1.0.0.2',
 	//适配最低CMS版本，
 	'adaptation' => '3.0.0.0',
 	//签名
